d24d90b0a29008d47c2e5c53f88f71198337e45f
Elizaveta0501 <110764428+Elizaveta0501@users.noreply.github.com>
2022-09-02 00:20:05 +0300

    Update script.js
    Добавила изменения в файл script.js

--- script.js
+++ script.js
@@ -13,3 +13,86 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+
+const forecastBtn = document.querySelector('.forecast-btn');
+
+const currentForecastName = document.querySelector('.current-forecast h1');
+
+const currentForecastResult = document.querySelector('.current-forecast p');
+
+const forecastsCase = document.querySelector('.forecasts');
+
+const forecastModel = document.querySelector('#forecast-item');
+
+function getRandomValue(min, max) {
+    return min + Math.floor(Math.random() * (max - min))
+}
+
+
+forecastBtn.addEventListener('click', function() {
+
+    addForecastToList();
+
+    generateNewForecast();
+})
+
+function generateForecastText(count) {
+    switch (count) {
+        case 0:
+            return "Немного терпения и желания сбудутся!"
+        case 1:
+            return "Все будет хорошо!";
+        case 2:
+            return "Счастье не за горами"
+        case 3:
+            return "Вас ждут Сейшельские острова!";
+        case 4:
+            return "Вам к лицу спортивная форма типа Adidas :)"
+        case 5:
+            return "У Вас появился тайный поклонник";
+        case 6:
+            return "Будет дома у тебя всегда вкусная еда!"
+        case 7:
+            return "Будет мир на земле и деликатесы на столе!";
+        case 8:
+            return "Будут у Вас торты, сладости и прочие мелкие радости!"
+        case 9:
+            return "Ждет Вас поездка в Европу и удача по гороскопу! :З";
+        default:
+            return "Доведите до конца начатое, Вас ждет сюрприз!";
+    }
+}
+
+
+function generateNewForecast() {
+
+    const randomValue = getRandomValue(0, 10);
+
+    const randomResult = getRandomValue(0, 101);
+
+    currentForecastName.textContent = generateForecastText(randomValue)
+
+    currentForecastResult.textContent = `Вероятность: ${randomResult}%`
+}
+
+function addForecastToList() {
+
+    const name = currentForecastName.textContent;
+
+    const result = currentForecastResult.textContent;
+
+    if (!name || !result) {
+
+        return;
+    }
+
+    const forecastToAdd = forecastModel.content.cloneNode(true);
+
+    forecastToAdd.querySelector('h3').textContent = name;
+
+    forecastToAdd.querySelector('p').textContent = result;
+
+    forecastsCase.prepend(forecastToAdd);
+
+}
